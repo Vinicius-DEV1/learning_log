2aac86aac720c26be488fbaebf1d922645a36564
Vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-01-23 10:23:26 -0300

    feat: topics view and display logic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 def index(request):
     """página principal do learning log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added') #query bd
+    context = {'topics': topics} #create dictionary
+    return render(request, 'learning_logs/topics.html', context)
