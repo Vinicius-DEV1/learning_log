7de833d9f470d63f6574cd380603c7458abbc5ed
Vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-01-22 21:57:54 -0300

    creating models and tables in db

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """um assunto sobre o qual o usuário está aprendendo"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def ___str___(self):
+        """devolve uma representação em string do modelo"""
+        return self.text
